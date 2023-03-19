7f0bacc58b6e34911a5ac35524bcdd61231389ea
qdl529 <qdl529@ku.alumni.dk>
2023-03-19 12:28:50 +0100

    Nu

--- inauguralproject/HouseholdSpecializationModel.py
+++ inauguralproject/HouseholdSpecializationModel.py
@@ -54,7 +54,13 @@ class HouseholdSpecializationModelClass:
         C = par.wM*LM + par.wF*LF
 
         # b. home production
-        H = HM**(1-par.alpha)*HF**par.alpha
+        if par.sigma = 1:
+            H = HM**(1-par.alpha)*HF**par.alpha
+        elif par.sigma = 0:
+            H = np.argmin(HM, HF)
+        else:
+            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**(par.sigma/(par.sigma-1)) 
+
 
         # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
