fb621374bcbfefc74bba73c30f2653754c0f035c
qdl529 <qdl529@ku.alumni.dk>
2023-03-19 18:54:01 +0100

    (y)

--- inauguralproject/HouseholdSpecializationModel.py
+++ inauguralproject/HouseholdSpecializationModel.py
@@ -116,9 +116,45 @@ class HouseholdSpecializationModelClass:
         return opt
 
     def solve(self,do_print=False):
-        """ solve model continously """
+        
+        par = self.par
+        sol = self.sol
+        opt = SimpleNamespace()
+        
+        # a. all possible choices
+        x = np.linspace(0,24, N)
+        LM,HM,LF,HF = np.meshgrid(x,x,x,x) # all combinations
+    
+        LM = LM.ravel() # vector
+        HM = HM.ravel()
+        LF = LF.ravel()
+        HF = HF.ravel()
 
-        pass    
+        # b. calculate utility
+        u = self.calc_utility(LM,HM,LF,HF)
+    
+        # c. set to minus infinity if constraint is broken
+        I = (LM+HM > 24) | (LF+HF > 24) # | is "or"
+        u[I] = -np.inf
+    
+        # d. find maximizing argument
+        j = np.argmax(u)
+        
+        opt.LM = LM[j]
+        opt.HM = HM[j]
+        opt.LF = LF[j]
+        opt.HF = HF[j]
+
+        # d1. finding HF/HM relationship
+        opt.HFHM = HF[j]/HM[j]
+
+        # e. print
+        if do_print:
+            for k,v in opt.__dict__.items():
+                print(f'{k} = {v:6.4f}')
+
+        return opt
+   
 
     def solve_wF_vec(self,discrete=False):
         """ solve model for vector of female wages """
