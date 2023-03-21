8a983914092aa34022c981e9502b9187d58619ba
qdl529 <qdl529@ku.alumni.dk>
2023-03-21 15:49:40 +0100

    Jonathan er kangen

--- inauguralproject/HouseholdSpecializationModel.py
+++ inauguralproject/HouseholdSpecializationModel.py
@@ -116,44 +116,38 @@ class HouseholdSpecializationModelClass:
         return opt
 
     def solve(self,do_print=False):
-        
+        """ solve model continously """
         par = self.par
-        sol = self.sol
+        sol = self.sol 
         opt = SimpleNamespace()
         
-        # a. all possible choices
-        x = np.linspace(0,24)
-        LM,HM,LF,HF = np.meshgrid(x,x,x,x) # all combinations
-    
-        LM = LM.ravel() # vector
-        HM = HM.ravel()
-        LF = LF.ravel()
-        HF = HF.ravel()
+        # a. guesses:
+        #LM,HM,LF,HF
+        LM_guess=6
+        HM_guess=6
+        LF_guess=6
+        HF_guess=6
+        x_guess=[LM_guess,HM_guess,LF_guess,HF_guess]
 
-        # b. calculate utility
-        u = self.calc_utility(LM,HM,LF,HF)
-    
-        # c. set to minus infinity if constraint is broken
-        I = (LM+HM > 24) | (LF+HF > 24) # | is "or"
-        u[I] = -np.inf
-    
-        # d. find maximizing argument
-        j = np.argmax(u)
+        # b. creating objective 
+        obj = lambda x: -self.calc_utility(x[0],x[1],x[2],x[3])
+
+        # c. creating bounds
+        bounds=[(0,24),(0,24),(0,24),(0,24)]
         
-        opt.LM = LM[j]
-        opt.HM = HM[j]
-        opt.LF = LF[j]
-        opt.HF = HF[j]
-
-        # d1. finding HF/HM relationship
-        opt.HFHM = HF[j]/HM[j]
+        # d. creating result element and extracting values from it
+        res = optimize.minimize(obj,x_guess,method='Nelder-Mead',bounds=bounds) 
+        opt.LM = res.x[0]
+        opt.HM = res.x[1]
+        opt.LF = res.x[2]
+        opt.HF = res.x[3]
 
         # e. print
         if do_print:
             for k,v in opt.__dict__.items():
                 print(f'{k} = {v:6.4f}')
 
-        return opt
+        return opt  
    
 
     def solve_wF_vec(self,discrete=False):
